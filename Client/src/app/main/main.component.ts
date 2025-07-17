a877d6fa576f1e5cb798c6e9275369d2fee19de9
Alen <alen.veselic01@gmail.com>
2025-07-17 20:44:50 +0200

    Improve api call response display and add users display.

--- Client/src/app/main/main.component.ts
+++ Client/src/app/main/main.component.ts
@@ -4,16 +4,17 @@ import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-main',
-    imports: [CommonModule],
-    templateUrl: './main.component.html',
-    styleUrl: './main.component.css'
+  selector: 'app-main',
+  imports: [CommonModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css',
 })
 export class MainComponent {
   authenticated = false;
   isUser = false;
   isAdmin = false;
   weatherData: any;
+  users: any;
 
   constructor(
     private authService: AuthorizationService,
@@ -25,29 +26,35 @@ export class MainComponent {
       console.log(roles.includes('ADMIN'));
       this.isUser = roles.includes('USER');
       this.isAdmin = roles.includes('ADMIN');
-
     }
   }
 
+  // ADMIN role from realm roles
   ngOnInit() {
     this.http.get('http://localhost:5159/weatherforecast').subscribe({
       next: (response) => {
         console.log(response);
-        this.weatherData =JSON.stringify(response);
+        this.weatherData = response;
       },
       error: (err) => {
         console.log(err);
-        this.weatherData = err;
+        this.weatherData = JSON.stringify(err);
       },
     });
     ///admin/realms
-    //  this.http.get('http://localhost:28080/admin/realms/my-realm/users').subscribe({
-    //   next: (response) =>{
-    //     console.log(response);
-    //   },
-    //   error: (err) => {
-    //     console.log(err);
-    //   }
-    //  });
+    // view-users role from client roles
+    this.http
+      .get<any[]>('http://localhost:28080/admin/realms/my-realm/users')
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+
+          this.users = response;
+        },
+        error: (err) => {
+          console.log(err);
+          this.users = JSON.stringify(err);
+        },
+      });
   }
 }
