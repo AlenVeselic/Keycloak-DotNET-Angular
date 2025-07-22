64bdcf66bc032dc34077d0fb99707065dd37394f
Alen <alen.veselic01@gmail.com>
2025-07-22 07:53:33 +0200

    Refactor main module

--- Client/src/app/main/main.component.ts
+++ Client/src/app/main/main.component.ts
@@ -3,6 +3,19 @@ import { AuthorizationService } from '../_auth/authorization.service';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
+enum UserRole {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+}
+
+enum RestApiEndpoint {
+  WEATHER_FORECAST = 'http://localhost:5159/weatherforecast',
+}
+
+enum KeycloakAdminEndpoint {
+  VIEW_USERS = 'http://localhost:28080/admin/realms/my-realm/users',
+}
+
 @Component({
   selector: 'app-main',
   imports: [CommonModule],
@@ -13,8 +26,12 @@ export class MainComponent {
   authenticated = false;
   isUser = false;
   isAdmin = false;
+
   weatherData: any;
+  weatherDataError = false;
+
   users: any;
+  usersError = false;
 
   constructor(
     private authService: AuthorizationService,
@@ -23,38 +40,39 @@ export class MainComponent {
     this.authenticated = this.authService.isLoggedIn();
     if (this.authenticated) {
       const roles = this.authService.getUserRoles();
-      console.log(roles.includes('ADMIN'));
-      this.isUser = roles.includes('USER');
-      this.isAdmin = roles.includes('ADMIN');
+      this.isUser = roles.includes(UserRole.USER);
+      this.isAdmin = roles.includes(UserRole.ADMIN);
     }
   }
 
   // ADMIN role from realm roles
   ngOnInit() {
-    this.http.get('http://localhost:5159/weatherforecast').subscribe({
+    this.http.get(RestApiEndpoint.WEATHER_FORECAST).subscribe({
       next: (response) => {
         console.log(response);
         this.weatherData = response;
       },
       error: (err) => {
         console.log(err);
-        this.weatherData = JSON.stringify(err);
+        this.weatherDataError = true;
+        this.weatherData = err;
       },
     });
-    ///admin/realms
-    // view-users role from client roles
-    this.http
-      .get<any[]>('http://localhost:28080/admin/realms/my-realm/users')
-      .subscribe({
-        next: (response) => {
-          console.log(response);
 
-          this.users = response;
-        },
-        error: (err) => {
-          console.log(err);
-          this.users = JSON.stringify(err);
-        },
-      });
+    ///admin/view-users
+    // view-users role from client roles
+    this.usersError = false;
+    this.http.get<any[]>(KeycloakAdminEndpoint.VIEW_USERS).subscribe({
+      next: (response) => {
+        console.log(response);
+
+        this.users = response;
+      },
+      error: (err) => {
+        console.log(err);
+        this.users = JSON.stringify(err);
+        this.usersError = true;
+      },
+    });
   }
 }
