d8e8d7b7a9c928dbe87e3443457ac33caae0580f
Alen <alen.veselic01@gmail.com>
2025-07-26 01:10:05 +0200

    Add user roles, remove some unecessary values from user table

--- Client/src/app/users/users.component.ts
+++ Client/src/app/users/users.component.ts
@@ -6,6 +6,7 @@ import { MatTableModule } from '@angular/material/table';
 
 enum KeycloakAdminEndpoint {
   VIEW_USERS = 'http://localhost:28080/admin/realms/my-realm/users',
+  USER_ROLES = 'http://localhost:28080/admin/realms/my-realm/users/{userId}/role-mappings',
 }
 @Component({
   selector: 'app-users',
@@ -29,7 +30,69 @@ export class UsersComponent {
       next: (response) => {
         this.users = response;
         if (this.users.length > 0) {
-          this.userKeys = Object.keys(this.users[0]);
+          delete this.users[0]['userProfileMetadata'];
+          delete this.users[0]['disableableCredentialTypes'];
+          delete this.users[0]['requiredActions'];
+          delete this.users[0]['access'];
+          delete this.users[0]['notBefore'];
+          this.userKeys = Object.keys({ ...this.users[0], roles: [] });
+        }
+
+        for (const user of this.users) {
+          // Remove sensitive information
+          delete user['credentials'];
+          delete user['access'];
+          delete user['federatedIdentities'];
+          delete user['requiredActions'];
+          delete user['attributes'];
+          delete user['clientConsents'];
+          delete user['userProfileMetadata'];
+
+          this.http
+            .get<any>(
+              KeycloakAdminEndpoint.USER_ROLES.replace('{userId}', user.id)
+            )
+            .subscribe({
+              next: (rolesResponse) => {
+                // Add roles to user object
+                user.roles = [];
+                if (
+                  rolesResponse.realmMappings &&
+                  rolesResponse.realmMappings.length > 0
+                ) {
+                  user.roles.push(
+                    ...(rolesResponse.realmMappings?.map(
+                      (role: { name: string }) => role.name
+                    ) || [])
+                  );
+                  console.log(
+                    'Realm roles for user:',
+                    user.username,
+                    user.roles
+                  );
+                }
+                if (
+                  rolesResponse.clientMappings &&
+                  rolesResponse.clientMappings['realm-management']
+                ) {
+                  user.roles.push(
+                    ...(rolesResponse.clientMappings[
+                      'realm-management'
+                    ]?.mappings.map((role: { name: string }) => role.name) ||
+                      [])
+                  );
+                  console.log(
+                    'Client roles for user:',
+                    user.username,
+                    user.roles
+                  );
+                }
+              },
+              error: (err) => {
+                console.error('Error fetching user roles:', err);
+                user.roles = []; // Ensure roles is always defined
+              },
+            });
         }
       },
       error: (err) => {
