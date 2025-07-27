854f07afdfe6f1d4d558028e816305e91f343102
Alen <alen.veselic01@gmail.com>
2025-07-27 12:32:47 +0200

    Make users component more readable by replacing the callback based implementatiowith a promise based one

--- Client/src/app/users/users.component.ts
+++ Client/src/app/users/users.component.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+import { lastValueFrom } from 'rxjs';
 
 enum KeycloakAdminEndpoint {
   VIEW_USERS = 'http://localhost:28080/admin/realms/my-realm/users',
@@ -33,81 +34,82 @@ export class UsersComponent {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.users = [];
     ///admin/view-users
     // view-users role from client roles
     this.usersError = false;
-    this.http.get<any[]>(KeycloakAdminEndpoint.VIEW_USERS).subscribe({
-      next: (response) => {
-        this.users = response;
-        if (this.users.length > 0) {
-          delete this.users[0]['userProfileMetadata'];
-          delete this.users[0]['disableableCredentialTypes'];
-          delete this.users[0]['requiredActions'];
-          delete this.users[0]['access'];
-          delete this.users[0]['notBefore'];
-          this.userKeys = [
-            ...this.regularCellKeys,
-            ...this.arrayCellKeys,
-            'actions',
-          ];
-        }
 
-        for (const user of this.users) {
-          // Remove sensitive information
-          delete user['credentials'];
-          delete user['access'];
-          delete user['federatedIdentities'];
-          delete user['requiredActions'];
-          delete user['attributes'];
-          delete user['clientConsents'];
-          delete user['userProfileMetadata'];
+    let users: any = [];
 
-          this.http
-            .get<any>(
-              KeycloakAdminEndpoint.USER_ROLES.replace('{userId}', user.id)
-            )
-            .subscribe({
-              next: (rolesResponse) => {
-                // Add roles to user object
-                user.roles = [];
-                if (
-                  rolesResponse.realmMappings &&
-                  rolesResponse.realmMappings.length > 0
-                ) {
-                  user.roles.push(
-                    ...(rolesResponse.realmMappings?.map(
-                      (role: { name: string }) => role.name
-                    ) || [])
-                  );
-                }
-                if (
-                  rolesResponse.clientMappings &&
-                  Object.values(rolesResponse.clientMappings).length > 0
-                ) {
-                  let clients = Object.keys(rolesResponse.clientMappings);
-                  for (const client of clients) {
-                    user.roles.push(
-                      ...(rolesResponse.clientMappings[client]?.mappings.map(
-                        (role: { name: string }) => role.name
-                      ) || [])
-                    );
-                  }
-                }
-                console.log('User roles:', user.roles);
-              },
-              error: (err) => {
-                console.error('Error fetching user roles:', err);
-                user.roles = []; // Ensure roles is always defined
-              },
-            });
+    try {
+      users = await lastValueFrom(
+        this.http.get<any[]>(KeycloakAdminEndpoint.VIEW_USERS)
+      );
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      this.usersError = true;
+      this.users = [];
+      return;
+    }
+
+    this.users = users;
+    if (this.users.length > 0) {
+      delete this.users[0]['userProfileMetadata'];
+      delete this.users[0]['disableableCredentialTypes'];
+      delete this.users[0]['requiredActions'];
+      delete this.users[0]['access'];
+      delete this.users[0]['notBefore'];
+      this.userKeys = [
+        ...this.regularCellKeys,
+        ...this.arrayCellKeys,
+        'actions',
+      ];
+    }
+
+    for (const user of this.users) {
+      // Remove sensitive information
+      delete user['credentials'];
+      delete user['access'];
+      delete user['federatedIdentities'];
+      delete user['requiredActions'];
+      delete user['attributes'];
+      delete user['clientConsents'];
+      delete user['userProfileMetadata'];
+
+      try {
+        let userRoles = await lastValueFrom(
+          this.http.get<any>(
+            KeycloakAdminEndpoint.USER_ROLES.replace('{userId}', user.id)
+          )
+        );
+        // Add roles to user object
+        user.roles = [];
+        if (userRoles.realmMappings && userRoles.realmMappings.length > 0) {
+          user.roles.push(
+            ...(userRoles.realmMappings?.map(
+              (role: { name: string }) => role.name
+            ) || [])
+          );
         }
-      },
-      error: (err) => {
-        this.users = JSON.stringify(err);
-        this.usersError = true;
-      },
-    });
+        if (
+          userRoles.clientMappings &&
+          Object.values(userRoles.clientMappings).length > 0
+        ) {
+          let clients = Object.keys(userRoles.clientMappings);
+          for (const client of clients) {
+            user.roles.push(
+              ...(userRoles.clientMappings[client]?.mappings.map(
+                (role: { name: string }) => role.name
+              ) || [])
+            );
+          }
+        }
+        console.log('User roles:', user.roles);
+      } catch (error) {
+        console.error('Error fetching roles for user:', user.id, error);
+        user.roles = []; // Ensure roles is always defined
+      }
+    }
   }
 }
