754348784ed1beb79b8fdd532d342771120ee0cc
Alen <alen.veselic01@gmail.com>
2025-09-26 22:48:42 +0200

    Add counter handling and fetching to profile

--- Client/src/app/profile/profile.component.ts
+++ Client/src/app/profile/profile.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RestApiEndpoint } from '../_rest/APIEndpoint';
+import { AddCounterDialogComponent } from './add-counter-dialog/add-counter-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-profile',
@@ -12,15 +14,76 @@ import { RestApiEndpoint } from '../_rest/APIEndpoint';
 export class ProfileComponent {
   profileData: { id: string; bio: string } | any;
   profileDataError = false;
-  constructor(private http: HttpClient) {}
+
+  counterData: { id: string; amount: string; name: string }[] | any;
+
+  constructor(private http: HttpClient, private dialog: MatDialog) {}
   ngOnInit() {
-    // USER role from realm roles
+    // Extended profile info from API
     this.http.get(RestApiEndpoint.GET_PROFILE).subscribe({
       next: (response) => {
         this.profileData = response;
       },
       error: (err) => {
         this.profileDataError = true;
+        console.log(err);
+      },
+    });
+
+    // Get user counters from API
+    this.fetchCounters();
+  }
+
+  createCounter() {
+    // Open dialog to create a new counter
+    console.log('Creating counter...');
+    const dialogRef = this.dialog.open(AddCounterDialogComponent, {
+      width: '300px',
+    });
+    // After dialog is closed, refresh the counter list
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.fetchCounters();
+    });
+
+    console.log('Counter creation dialog closed');
+  }
+
+  incrementCounter(counterId: string) {
+    const endpoint = RestApiEndpoint.UPDATE_COUNTER_AMOUNT.replace(
+      '{id}',
+      counterId
+    );
+    const currentAmount = this.counterData.find(
+      (c: any) => c.id === counterId
+    ).amount;
+
+    const incrementedAmount = Number.parseInt(currentAmount) + 1;
+
+    const updateData = {
+      amount: incrementedAmount.toString(),
+    };
+    this.http.put<{ amount: string }>(endpoint, updateData).subscribe({
+      next: (response: { amount: string }) => {
+        console.log('Counter updated:', response);
+        const counter = this.counterData.find((c: any) => c.id === counterId);
+        if (counter) {
+          counter.amount = response.amount;
+        }
+      },
+      error: (err) => {
+        console.log('Error updating counter:', err);
+      },
+    });
+  }
+
+  fetchCounters() {
+    this.http.get(RestApiEndpoint.GET_USER_COUNTERS).subscribe({
+      next: (response) => {
+        this.counterData = response;
+        console.log(response);
+      },
+      error: (err) => {
         console.log(err);
       },
     });
