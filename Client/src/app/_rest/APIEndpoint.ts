0afb8ba913bbb1555fc417495003cbd7144d8897
Alen <alen.veselic01@gmail.com>
2025-09-26 22:45:20 +0200

    Add new endpoints to Client

--- Client/src/app/_rest/APIEndpoint.ts
+++ Client/src/app/_rest/APIEndpoint.ts
@@ -1,5 +1,8 @@
 enum RestApiEndpoint {
   WEATHER_FORECAST = 'http://localhost:5159/weatherforecast',
   GET_PROFILE = 'http://localhost:5159/user',
+  GET_USER_COUNTERS = 'http://localhost:5159/counter/counters',
+  CREATE_COUNTER = 'http://localhost:5159/counter/create',
+  UPDATE_COUNTER_AMOUNT = 'http://localhost:5159/counter/{id}',
 }
 export { RestApiEndpoint };
